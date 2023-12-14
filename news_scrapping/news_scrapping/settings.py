d9b0e1abde30ab52e1ceed3aa9ea42909ae58ac4
ketan <ketankatty11bamn@gmail.com>
2023-12-14 16:34:44 +0530

    Allow 0.0.0.8000 port to django

--- news_scrapping/news_scrapping/settings.py
+++ news_scrapping/news_scrapping/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l(q)3)!gj_$*7j=l__pus#!vig7n(#jxr%uab*22#gkf+=ab=2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
